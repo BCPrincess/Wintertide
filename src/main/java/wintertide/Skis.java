21baeb0f69eb58dbc722f66a276979e93a5d1271
Alex Nauda <alex@alexnauda.com>
2014-05-04 19:21:26 -0400

    fix skis icon and crafting crash

--- src/main/java/wintertide/Skis.java
+++ src/main/java/wintertide/Skis.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.util.IIcon;
 
 public class Skis extends Item {
 
@@ -12,6 +13,10 @@ public class Skis extends Item {
 		setMaxStackSize(1);
 		setCreativeTab(CreativeTabs.tabMisc);
 		setUnlocalizedName("skis");
-		setTextureName("wintertide:skis");
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister) {
+	                 this.itemIcon = par1IconRegister.registerIcon("wintertide:skis");
 	}
 }
--- src/main/java/wintertide/Wintertide.java
+++ src/main/java/wintertide/Wintertide.java
@@ -3,26 +3,41 @@ package wintertide;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = "wintertide", name = "Wintertide", version = "0.0.1")
 public class Wintertide {
+	
+	public String modid = "wintertide";
+	
+    @Instance("wintertide")
+    public static Wintertide instance;
+
+    @SidedProxy(clientSide = "wintertide.ClientProxy", serverSide = "wintertide.CommonProxy")
+    public static CommonProxy proxy;
+	
+    private static Skis skis = new Skis();
+    private static ItemStack skisStack = new ItemStack(skis, 1);
+    private static ItemStack planksStack = new ItemStack(Blocks.planks, 1);
+    
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent preInitEvent) {
-	        Skis skis = new Skis();
-	        skis.setTextureName("wintertide:skis");
-	        GameRegistry.registerItem(skis, skis.getUnlocalizedName(), "wintertide");
+	        GameRegistry.registerItem(skis, skis.getUnlocalizedName(), modid);
 	}
 	
 	@EventHandler
     public void load(FMLInitializationEvent event) {
-		GameRegistry.addRecipe(new ItemStack(new Skis(), 1),
+        proxy.registerRenderers();
+		
+		GameRegistry.addRecipe(skisStack,
 				"p p",
 				"p p",
 				"p p",
-                'p', new ItemStack(Blocks.planks));
+                'p', planksStack);
 	}
 }
